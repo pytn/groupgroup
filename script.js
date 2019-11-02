98da911cb06015cea49281070906e68f98f23ef2
honeyhands <57265524+honeyhands@users.noreply.github.com>
2019-11-02 14:14:08 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
 // Javascript for the ~pages~
 // CS220
 
+//Popover Code
 $(document).ready(function(){
   $('[data-toggle="popover"]').popover();
 });
 $('.popover-dismiss').popover({
   trigger: 'focus'
 })
+
+//Drop down search code
+$(document).ready(function(){
+  $("#myInput").on("keyup", function() {
+    var value = $(this).val().toLowerCase();
+    $(".dropdown-menu li").filter(function() {
+      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+    });
+  });
+});
