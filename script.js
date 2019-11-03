158c4c831dce80d9566c64504c48d2a68653f5db
honeyhands <57265524+honeyhands@users.noreply.github.com>
2019-11-03 14:17:26 -0500

    Update script.js

--- script.js
+++ script.js
@@ -19,18 +19,19 @@ $(document).ready(function(){
   });
 });
 
-
 function alertFunc(){
-  alert("Hello! You have successfully registered for:\n AMST 274 \nBISC 111 \nBISC 111-L01 \nAFR 211 \nYou were NOT registered for CAMS 101. If you want to be register for another course, please select one from your saved course list or browse other courses!")
+  alert("Hello! You have successfully registered for:\n AMST 274 \nBISC 111 \nBISC 111-L01 \nAFR 211 \nCAMS 101")
   //if checkbox is checked, remove it.
   var boxes = document.getElementsByClassName('chk');
-  //var texts = document.getElementsByClassName('txt');
-   for(var i = 0; i<boxes.length; i++){
+  var texts = document.getElementsByClassName('txt');
+
+   for(i=0; i<boxes.length; i++ ){
        box = boxes[i];
-       //txt = texts[i];
+       txt = texts[i];
        if(box.checked){
            box.parentNode.removeChild(box);
-           //txt.parentNode.removeChild(txt);
+           txt.parentNode.removeChild(txt);
+           i=i-1;
        }
    }
 }
